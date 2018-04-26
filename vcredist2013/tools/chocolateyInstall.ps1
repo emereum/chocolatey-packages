7e78d3de87c9cbe8f67b13d5878e57a41b86298b
hoflogic <hoflogic@users.noreply.github.com>
2018-04-27 08:14:39 +1000

    Suppress restart for vcredist2013

--- vcredist2013/tools/chocolateyInstall.ps1
+++ vcredist2013/tools/chocolateyInstall.ps1
@@ -6,7 +6,7 @@ $checksumType = 'sha256'
 $url64 = 'https://download.microsoft.com/download/0/5/6/056dcda9-d667-4e27-8001-8a0c6971d6b1/vcredist_x64.exe'
 $checksum64 = '20e2645b7cd5873b1fa3462b99a665ac8d6e14aae83ded9d875fea35ffdd7d7e'
 $checksumType64 = 'sha256'
-$silentArgs = '/Q'
+$silentArgs = '/Q /norestart'
 $validExitCodes = @(0,3010)  # http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
 
 Install-ChocolateyPackage -PackageName "$packageName" `
